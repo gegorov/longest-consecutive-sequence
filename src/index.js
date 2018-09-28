3ac4f66bdb7e2d69690d8e13929bbd6fccd5d931
gegorov <gegorov@yahoo.com>
2018-09-29 02:01:05 +0300

    Longest Sequence: solution added

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function longestConsecutiveLength(array) {
   // your solution here
-}
+  if (array.length === 0) {
+    return 0;
+  }
+  const set = new Set(array);
+  let longestSeq = 0;
+  for (const number of set) {
+    if (!set.has(number - 1)) {
+      let current = number;
+      let counter = 1;
+
+      while (set.has(current + 1)) {
+        current += 1;
+        counter += 1;
+      }
+
+      longestSeq = longestSeq >= counter ? longestSeq : counter;
+    }
+  }
+  return longestSeq;
+};
